c06220eb18eaeee94562bab896fe70f2cb16983c
404 <3216065752@qq.com>
2025-10-09 10:16:43 +0800

    2025-10-9/1

--- 15 --- 后端 --- LogBack（日志框架）/SB_LOG/ch12/src/main/java/com/example/ch12/common/config/RabbitConfig.java
+++ 15 --- 后端 --- LogBack（日志框架）/SB_LOG/ch12/src/main/java/com/example/ch12/common/config/RabbitConfig.java
@@ -6,21 +6,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-
+    // 队列名
     public static final String ORDER_QUEUE = "order-queue";
+    // 交换机名
     public static final String ORDER_EXCHANGE = "order-exchange";
+    // 路由钥匙，匹配队列与交换机的
     public static final String ORDER_ROUTING_KEY = "order";
 
+    // 创建一个非持久化队列
     @Bean
     public Queue orderQueue() {
         return QueueBuilder.nonDurable(ORDER_QUEUE).build();
     }
 
+    // 创建一个非持久化，没有队列绑定后会删除的交换机
     @Bean
     public DirectExchange orderExchange() {
         return new DirectExchange(ORDER_EXCHANGE, false, false);
     }
 
+    // 绑定队列与交换机
     @Bean
     public Binding orderBinding(Queue orderQueue, DirectExchange orderExchange) {
         return BindingBuilder.bind(orderQueue).to(orderExchange).with(ORDER_ROUTING_KEY);
